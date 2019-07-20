b355425b8e0935fd4a3ffc0ba0d910e03896df44
github username <gregm9999@gmail.com>
2019-07-20 16:12:34 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-// ./src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import shoppingListItemReducer from "./reducers/shoppingListItemReducer";
+import App from "./App";
+import "./index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; /* code change */
-import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
-import App from './App';
-import './index.css';
+const store = createStore(
+  shoppingListItemReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={store} />, document.getElementById("root"));
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,14 +1,20 @@
 export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
+  state = {
+    items: []
+  },
+  action
 ) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
+  console.log(action);
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      console.log("Current state.items length %s", state.items.length);
+      console.log("Updating state.items length to %s", state.items.length + 1);
+      return Object.assign({}, state, {
+        items: state.items.concat(state.items.length + 1)
+      });
 
-		default:
-			return state;
-	}
+    default:
+      console.log("Initial state.items length: %s", state.items.length);
+      return state;
+  }
 }
